212df3b5b039c40f03272bc97064942d210231ca
Yiwei Liu <lywei.dinpay@gmail.com>
2018-09-06 12:06:20 +0800

    selection sort

---
+++ src/main/java/com/openmind/sort/BaseTestSort.java
@@ -0,0 +1,47 @@
+package com.openmind.sort;
+
+public abstract class BaseTestSort {
+
+    abstract int[] sort(int[] origin);
+
+    static int[] genRandomArray(int count) {
+        if (count == 0) {
+            count = 7;
+        }
+
+        int[] origin = new int[count];
+        for (int i = 0; i < count; i++) {
+            origin[i] = (int) (Math.random() * 100);
+        }
+
+        return origin;
+    }
+
+    static void printArray(int[] origin) {
+        for (int t : origin) {
+            System.out.print(t + " ");
+        }
+        System.out.println();
+    }
+
+    void proceeding(int n) {
+
+        //init array, then print
+        int[] origin = genRandomArray(n);
+        System.out.print("Origin: ");
+        printArray(origin);
+
+        //sorting & print
+        origin = sort(origin);
+        System.out.print("Sorted: ");
+        printArray(origin);
+    }
+
+    void swap(int[] a, int i, int j) {
+        int tmp;
+        tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+
+    }
+}
---
+++ src/main/java/com/openmind/sort/SimpleSelectionSort.java
@@ -0,0 +1,37 @@
+package com.openmind.sort;
+
+/**
+ * 简单选择排序 Selection Sort
+ * complex: O(n^2)
+ * storage: O(n)
+ */
+public class SimpleSelectionSort extends BaseTestSort {
+
+    @Override
+    int[] sort(int[] a) {
+        if (a == null || a.length == 0) {
+            return a;
+        }
+
+        int min, count = 0, n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[min]) {
+                    min = j;
+                }
+                count++;
+            }
+            if (min != i) {
+                swap(a, i, min);
+            }
+        }
+        System.out.printf("element: %s, O(n) = %s \n", n, count);  // n-1 + n-2 + …… + 1 = n (n -1 ) /2
+        return a;
+    }
+
+    public static void main(String[] args) {
+        new SimpleSelectionSort().proceeding(10);
+    }
+
+}
