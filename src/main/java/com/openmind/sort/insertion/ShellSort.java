cc13ea31e0d88693b916244ace79fea1d42c56b8
Yiwei Liu <lywei.dinpay@gmail.com>
2018-09-29 09:44:50 +0800

    upate shell sort

--- src/main/java/com/openmind/sort/insertion/ShellSort.java
+++ src/main/java/com/openmind/sort/insertion/ShellSort.java
@@ -3,29 +3,29 @@ package com.openmind.sort.insertion;
 import com.openmind.sort.BaseSortProcess;
 
 /**
- * Shell Sort:
- * CPU complexity:
- * Mem complexity:
+ * Shell Sort: ascending , Diminishing Increment Sort
+ * U complexity: O(n^（1.3—2）)
+ * Mem complexity: O(1)
+ * Unstable
  */
 public class ShellSort extends BaseSortProcess {
 
     @Override
     protected int[] sort(int[] a) {
-
         int n = a.length;
+
         //(n/2, n/2/2, ..., 1)
         for (int gap = n / 2; gap > 0; gap /= 2) {
-            //sub array insertion sort
+
+            //insertion sort
             for (int i = gap; i < n; i++) {
                 int j = i, tmp = a[j];
-//                if (a[j] < a[j - gap]) {
-                    while (j - gap >= 0 && tmp < a[j - gap]) {
-                        a[j] = a[j - gap];
-                        j -= gap;
-                    }
-                    a[j] = tmp;
-//                    printArray(a);
-//                }
+                while (j - gap >= 0 && tmp < a[j - gap]) {
+                    a[j] = a[j - gap];
+                    j -= gap;
+                }
+                a[j] = tmp;
+                printArray(a);
             }
             System.out.println();
         }
@@ -39,6 +39,6 @@ public class ShellSort extends BaseSortProcess {
     }
 
     public static void main(String[] args) {
-        new ShellSort().proceeding(100);
+        new ShellSort().proceeding(10);
     }
 }
