d351549b8d0ffc8040c8e9287c572381f0cee4c9
Yiwei Liu <lywei@dinpay.com>
2018-10-20 18:56:12 +0800

    avl tree deletion

--- src/main/java/com/openmind/tree/AVLTree.java
+++ src/main/java/com/openmind/tree/AVLTree.java
@@ -35,6 +35,15 @@ public class AVLTree {
         return height(node.left) - height(node.right);
     }
 
+    Node minValueNode(Node node) {
+        Node current = node;
+        while (current.left != null) {
+            current = current.left;
+        }
+
+        return current;
+    }
+
     //case: left-left
     Node rightRotate(Node root) {
         Node lNode = root.left;
@@ -115,6 +124,68 @@ public class AVLTree {
         return node;
     }
 
+    Node delete(Node root, int key) {
+        if (root == null) {
+            return root;
+        }
+
+        if (key < root.key) {
+            root.left = delete(root.left, key);
+        } else if (key > root.key) {
+            root.right = delete(root.right, key);
+        } else {
+            if (root.left == null || root.right == null) {
+                Node tmp = root.left != null ? root.left : root.right;
+                if (tmp != null) {
+                    //case: one child
+                    root = tmp;
+                } else {
+                    //case: no child
+                    tmp = root;
+                    root = null;
+                }
+            } else {
+                //case: two child
+                Node tmp = minValueNode(root.right);
+                root.key = tmp.key;
+                //remove the min value node  of right-subtree
+                root.right = delete(root.right, tmp.key);
+            }
+
+        }
+
+        if (root == null) {
+            return root;
+        }
+
+
+        root.height = max(height(root.left), height(root.right)) + 1;
+        int balance = getBalance(root);
+
+        //case : left - left
+        if (balance > 1 && (getBalance(root.left) > 0)) {
+            return rightLeftRotate(root);
+        }
+
+        //case: left -right
+        if (balance > 1 && (getBalance(root.left) < 0)) {
+            return leftRightRotate(root);
+        }
+
+        //case: right -left
+        if (balance < -1 && getBalance(root.right) > 0) {
+            return rightLeftRotate(root);
+        }
+
+        //case: right -right
+        if (balance < -1 && getBalance(root.right) < 0) {
+            return leftRorate(root);
+        }
+
+        return root;
+    }
+
+
     void preOrder(Node node) {
         if (node == null) {
             return;
@@ -174,8 +245,13 @@ public class AVLTree {
         avl.root = avl.insert(avl.root, 8);
         avl.preOrder(avl.root);
         System.out.println();
-        avl.inOrder(avl.root);
-        System.out.println();
-        avl.postOrder(avl.root);
+//        avl.inOrder(avl.root);
+//        System.out.println();
+//        avl.postOrder(avl.root);
+
+//        avl.delete(avl.root, 8);
+//        avl.delete(avl.root, 7);
+        avl.delete(avl.root, 6);
+        avl.preOrder(avl.root);
     }
 }
